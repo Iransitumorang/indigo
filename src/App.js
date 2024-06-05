2a4154d5baa3a2fcc3c54876792e7ced574400d3
Iran Situmorang <iran.situmorang@iconpln.co.id>
2024-06-05 19:31:39 +0700

    indigo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [dataApi, setDataApi] = useState([]);
+
+  // Menggunakan Axios
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        setDataApi(res.data);
+      })
+  }, []); 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: 'green', borderRadius: '10px'}}>
+      <h2 align="center" style={{paddingTop: '25px'}}>Creative Indigo</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        {dataApi.map((data, index) => {
+          return(
+            <div key={index} style={{ background: 'grey', margin: '20px', padding: '15px', width: '20%', borderRadius: '10px', boxShadow: '10px 10px', fontFamily: 'sans-serif'}}>
+              <div>ID : {data.id} </div> 
+              <div style={{margin: '5px auto'}}>Name: {data.name} </div> 
+              <div>Username: {data.username} </div> 
+              <div style={{margin: '5px auto'}}>Email : {data.email} </div> 
+              <div>City : {data.address.city} </div> 
+              <div style={{margin: '5px auto'}}>Website : {data.website} </div> 
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
